1bcf78d087f3905b0a01a795399bb695d4365eb3
marvin-c <marboynel@gmail.com>
2019-11-19 15:51:38 +1300

    conversion from binary to lotto combination

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ let binary = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 let n
 let arr_nums_length = 40
 let lottoCombination = []
+let lottoIndex
 
 for (n=1; n<=6; n++) {
   let i = Math.floor((Math.random() * (arr_nums_length)))
   lottoCombination.push(arrNumbers[i])
-  let lottoIndex = lottoCombination[n-1]
-  binary[lottoIndex - 1] = 1
+  lottoIndex = lottoCombination[n-1]
+  convertNumToBit(lottoIndex)
   arr_nums_length--
   arrNumbers.splice(i, 1)
 }
@@ -17,5 +18,22 @@ binary = binary.join()
 binary = binary.split(',').join("")
 console.log(lottoCombination)
 console.log(binary)
+convertBinarytoNumberCombination(binary)
 
-let yyy = binary.length
+function convertBinarytoNumberCombination (bin) {
+  arrNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
+  binary = bin.split('')
+  lottoCombination = []
+  let tempLottoCombination = binary.map(function (value, index){
+    // console.log(value, index)
+    if (value === '1') {
+      lottoCombination.push(arrNumbers[index])
+      // console.log(arrNumbers[index+1], '----')
+    }
+  }) 
+  console.log(lottoCombination)
+}
+
+function convertNumToBit (num) {
+  binary[lottoIndex - 1] = 1
+}
