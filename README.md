1c56929245836678ceacf9c8b5dfd04e439d67f1
marvin-c <marboynel@gmail.com>
2019-11-19 14:54:32 +1300

    conversion from lotto combination to binary

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Lotto-Analysis
+
+
+Description
+
+    Java/C#/C++/any Language you are familiar with.
+    Step up Tasks
+
+    Create a function to print all possible lotto numbers groups. (Lotto select roll: Pick 6 numbers as a group, all numbers are selected from 1 to 40, no same numbers in a group.
+    Make each group numbers record style into a binary number as a string.(Example: a group: 1,5,7,24,34,38 into binary style string will be 1000101000000000000000010000000001000100; the length of the binary number is 40, each digit meaning for the number of a lotto ball.)
+    Make each Binary Number into Decimal Number. (Example: 1000101000000000000000010000000001000100=)
+    Create a Database to store all data;
+    Frontend UI & Functions:
+
+    Being able to Categorize the regularity of numbers groups. (Example: 10101010101 or 111111 or 1110111 or 111%111)
+    Make the function that can aloud you to insert a group of numbers then compare with the database;
+    Make a function for existing numbers that have happen previously, make a excel file import function to import the data of all existing numbers groups and also mark up them with the binary string and decimal number, by the importing and inserting, such numbers will also be marked on all possible groups record.
+    Creatively make some function for the data analysis, to support on number pick. The regularity of numbers groups is one of this kind. So think of something that you can add on.
+
+    Complete the system and package it as installable software.
+
+    Extended Ideas
+
+If you have any idea, you can add to this system as you made. For the time limit, you may not be able to complete it, so please list it up and present it to the examiner. This is on the purpose of knowing your creativity.
+
+    Documentation
+
+Please do keep up the documentation for your program.
--- index.js
+++ index.js
@@ -1,84 +1,21 @@
-let arrNumbers = [
-  [1,'000001'],
-  [2,'000010'],
-  [3,'000011'],
-  [4,'000100'],  
-  [5,'000101'],
-  [6,'000110'],
-  [7,'000111'],
-  [8,'001000'],
-  [9,'001001'],
-  [10,'001010'],
-  [11,'001011'],
-  [12,'001100'],
-  [13,'001101'],
-  [14,'001110'],
-  [15,'001111'],
-  [16,'010000'],
-  [17,'010001'],
-  [18,'010010'],
-  [19,'010011'],
-  [20,'010100'],
-  [21,'010101'],
-  [22,'010110'],
-  [23,'010111'],
-  [24,'011000'],
-  [25,'011001'],
-  [26,'011010'],
-  [27,'011011'],
-  [28,'011100'],
-  [29,'011101'],
-  [30,'011110'],
-  [31,'011111'],
-  [32,'100000'],
-  [33,'100001'],
-  [34,'100010'],
-  [35,'100011'],
-  [36,'100100'],
-  [37,'100101'],
-  [38,'100110'],
-  [39,'100111'],
-  [40,'101000']
-]
-
-// let arrNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
-//                  1        2        3        4        5        6        7        8        9        10        11      12      13      14         15      16        17       18      19          20      21      22         23      24      25        26        27      28      29        30       31       32       33        34      35       36       37      38        39      40
-// let arrBinary = ['000001','000010','000011','000100','000101','000110','000111','001000','001001','001010','001011','001100','001101','001110','001111','010000','010001','010010','010011','010100','010101','010110','010111','011000','011001','011010','011011','011100','011101','011110','011111','100000','100001','100010','100011','100100','100101','100110','100111','101000']
-
-// let n
-// let arr_nums_length = 40
-// let lottoCombination = []
-// let lottoCombinationBinary = []
-
-// for (n=1; n<=6; n++) [
-//   let i = Math.floor((Math.random() * (arr_nums_length)))
-//   lottoCombination.push(arrNumbers[i])
-//   lottoCombinationBinary.push(arrBinary[i])
-//   arr_nums_length--
-//   arrNumbers.splice(i, 1)
-//   arrBinary.splice(i, 1)
-// ]
-
-// console.log(lottoCombination)
-// console.log(lottoCombinationBinary)
-
+let arrNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
+let binary = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 let n
 let arr_nums_length = 40
 let lottoCombination = []
-let lottoCombinationBinary = []
 
 for (n=1; n<=6; n++) {
   let i = Math.floor((Math.random() * (arr_nums_length)))
-  lottoCombination.push(arrNumbers[i][0])
-  lottoCombinationBinary.push(arrNumbers[i][1])
+  lottoCombination.push(arrNumbers[i])
+  let lottoIndex = lottoCombination[n-1]
+  binary[lottoIndex - 1] = 1
   arr_nums_length--
   arrNumbers.splice(i, 1)
-  // arrBinary.splice(i, 1)
 }
-console.log(lottoCombination)
-console.log(lottoCombinationBinary)
-lottoCombinationBinary = lottoCombinationBinary.join()
-lottoCombinationBinary = lottoCombinationBinary.split(',').join("")
-console.log(lottoCombinationBinary)
 
-// console.log(numbers[1][1])
+binary = binary.join()
+binary = binary.split(',').join("")
+console.log(lottoCombination)
+console.log(binary)
+
+let yyy = binary.length
